8cf278f7997a7a5a487c3ae87b5b88c2db92c582
Fully34 <cfullinw34@gmail.com>
2015-09-17 17:47:05 -0600

    can find the input, but have not figured out how to do the path or break out of the function

--- script.js
+++ script.js
@@ -152,33 +152,104 @@ var obj = {
   "facebookProfile" : null
 }
 
+var obj2 = {"dlpgcack": false, "indwqahe": null, "caki": {"vvczskh": null, "tczqyzn": 
+false, "qymizftua": "jfx", "cyd": {"qembsejm": [null, "dailyprogrammer", null], 
+"qtcgujuki": 79, "ptlwe": "lrvogzcpw", "jivdwnqi": null, "nzjlfax": "xaiuf", 
+"cqajfbn": true}, "kbttv": "dapsvkdnxm", "gcfv": 43.25503357696589}, "cfqnknrm": 
+null, "dtqx": "psuyc", "zkhreog": [null, {"txrhgu": false, "qkhe": false, 
+"oqlzgmtmx": "xndcy", "khuwjmktox": 48, "yoe": true, "xode": "hzxfgvw", 
+"cgsciipn": 20.075297532268902}, "hducqtvon", false, [null, 76.8463226047357, 
+"qctvnvo", null], [null, {"nlp": false, "xebvtnvwbb": null, "uhfikxc": null, 
+"eekejwjbe": false, "jmrkaqky": null, "oeyystp": false}, [null, 10, "nyzfhaps", 
+71, null], 40, null, 13.737832677566875], [true, 80, 20, {"weynlgnfro":
+40.25989193717965, "ggsirrt": 17, "ztvbcpsba": 12, "mljfh": false, "lihndukg": 
+"bzebyljg", "pllpche": null}, null, [true, false, 52.532666161803895, "mkmqrhg",
+ "kgdqstfn", null, "szse"], null, {"qkhfufrgac": "vpmiicarn", "hguztz": 
+ "ocbmzpzon", "wprnlua": null}], {"drnj": [null, false], "jkjzvjuiw": false, 
+ "oupsmgjd": false, "kcwjy": null}]}
+
+
 var module = {
   path       : [],
 
   findString : function(input, object) {
 
+    var found = false;
+
     for (key in object) {
 
-      if (object[key].constructor === Object || object[key].constructor === Array) {
-        
-        console.log(object[key].constructor)
+      //> make sure we aren't looking at null value
+      if (object[key] !== null) {
 
+        // if we find the input, we stop
+        if(object[key] === input) {
+
+          console.log("it's here: ", object, object[key]);
+          return 'property';
+        }
+
+        //============================== if the key is an object ==============================//
+
+        //> if the value is an object, we run the function on that sub-object
         if (object[key].constructor === Object) {
-          
+
           // run the function on the value of the key
-          module.findString(input, key);
-          console.log("object: ", key);
+          // debugger;
+          console.log("sub-object: ",key, object[key]);
+
+          //> push the key into the path array
+          // module.path.push(key);
+
+          module.findString(input, object[key]);
         };
 
-        if (object[key].constructor === Array) {
+        //============================== if the key is an array ==============================//
+
+        //> if the value is an array, we loop through it and look at each index
+        if (object[key] !== null && object[key] !== undefined && object[key].constructor === Array) {
+
+          var arr = object[key];
+
+          console.log("array: ", key);
 
           // check through the values in the array
-          console.log("array: ", key);
-        }
-      }
+          for (var i = 0; i < arr.length; i++) {
 
+            // debugger;
+            if (arr[i] !== null && arr[i].constructor === Object) {
 
-    }
+              //> push the name of the key into the path array
+              module.path.push(key);
+
+              module.findString(input, arr[i]);
+            }
+
+            if (arr[i] === input) {
+
+              // return 'from array';
+              module.path.push(key);
+              module.path.push(i);
+            }
+          }; //> end for 
+
+        } //> end array check;
+      }//> end null check
+    } //> end for in
+  },
+
+  printPath : function(input, object) {
+
+    var path = '';
+
+    module.findString(input,object);
+
+    for (var i = 0; i < module.path.length; i++) {
+
+      path += module.path[i] + " -> "
+    };
+
+    module.path = [];
+    return path;
   }
 };
 
